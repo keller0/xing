66ce1a7c0d3ca864117d96674844305909429ba2
caijiaqi <caijiaqi2017@gmail.com>
2020-11-26 00:07:33 +0800

    gorm tags use ';'

--- server/storage/stuff.go
+++ server/storage/stuff.go
@@ -6,5 +6,5 @@ type Notes struct {
 	Id         string    `json:"id" gorm:"column:id"`
 	Uid        string    `json:"uid" gorm:"column:uid"`
 	Content    string    `json:"content" gorm:"column:content"`
-	CreateTime time.Time `json:"create_time" gorm:"column:create_time type:datetime"`
+	CreateTime time.Time `json:"create_time" gorm:"column:create_time; type:datetime"`
 }
